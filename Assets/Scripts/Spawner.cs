df3366c6fef3c4c325cc60be7f0de19be971c94b
Kol4mo <47543776+kol4mo@users.noreply.github.com>
2024-03-28 16:35:39 -0600

    added ragdoll

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+    [SerializeField] KeyCode action;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(action)) {
+            Instantiate(prefab, transform.position, Quaternion.identity);
+        }
+    }
+}
